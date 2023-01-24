8c27a6091331d550de18916f5d14e90e1e7b17ee
wfeng <wfeng7857@gmail.com>
2023-01-25 03:02:07 +0530

    Added multithreading

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import tweepy
-import time
-from scraper import tips_and_resources
+from scraper import scraper
+from threading import Thread
+from tweeter import tweeting
 
-# Authenticate with Twitter API
-auth = tweepy.OAuth1UserHandler("cosumer-key", "consumer-secret", "access-token", "access-token-secret")
-api = tweepy.API(auth)
+# Create threads
 
-# Post a new tip or resource every hour
-while True:
-    for tip_or_resource in tips_and_resources:
-        api.update_status(tip_or_resource)
-        time.sleep(3600)
+scraper_thread = Thread(target=scraper)
+main_thread = Thread(target=tweeting)
+
+# Start threads
+scraper_thread.start()
+main_thread.start()
+
+# Join threads
+scraper_thread.join()
+main_thread.join()
