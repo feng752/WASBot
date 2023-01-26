e2d3f44c93572412ddfe7ef8c8118f7850fc5e63
wfeng <wfeng7857@gmail.com>
2023-01-26 16:13:58 +0530

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -5,16 +5,16 @@ from scraper import tips_and_resources
 
 def tweeting():
 
-    consumer_key = ""
-    consumer_secret = ""
-    access_token = ""
-    access_token_secret = ""
+    consumer_key = "Ns11du5d94nkPUhAGRe8wgjow"
+    consumer_secret = "x6eJ9UbGGpJMaB34oscMJHVEkRpvo6eHNy3LYMtny4caaZMU5y"
+    access_token = "3165048462-AybyEBO46WVsLaoEwzrahrXjoq2zu0vampZ2Z0t"
+    access_token_secret = "QdZm8cuv41lTY51UUsDgbK6Hi2KYhYGVrO9qvCgzJjeXv"
 
     # Authenticate with Twitter API
     auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    tweets = api.user_timeline(screen_name="")
+    # tweets = api.user_timeline(screen_name="nerdisland1728")
 
     tweeted = set()  # Create a set to store previously tweeted articles
 
@@ -32,8 +32,8 @@ def tweeting():
 
             if not tweet_exists:  # If tweet hasn't been tweeted, tweet it
                 try:
-                    api.update_status(item["title"] + "\n" + item["link"] + "\n#gamedev #indiegames #gamingnews #gamingindustry #gamebot")
-                    time.sleep(3600)
+                    api.update_status(item["title"] + "\n" + item["link"] + "\n#gamedev #indiegames #gamingnews #gamingindustry #gamebot" + " #" + item['title'].split(" ", 1)[0].lower())
+                    time.sleep(20)
                 except tweepy.errors.TweepyException as e:  # Exception handling
                     if e == 187:
                         print("Duplicate tweet.\n")
