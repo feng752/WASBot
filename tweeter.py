f126d9913338e8064aee09305caf843d3bda99ff
wfeng <wfeng7857@gmail.com>
2023-01-26 16:14:45 +0530

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -5,16 +5,14 @@ from scraper import tips_and_resources
 
 def tweeting():
 
-    consumer_key = "Ns11du5d94nkPUhAGRe8wgjow"
-    consumer_secret = "x6eJ9UbGGpJMaB34oscMJHVEkRpvo6eHNy3LYMtny4caaZMU5y"
-    access_token = "3165048462-AybyEBO46WVsLaoEwzrahrXjoq2zu0vampZ2Z0t"
-    access_token_secret = "QdZm8cuv41lTY51UUsDgbK6Hi2KYhYGVrO9qvCgzJjeXv"
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
 
     # Authenticate with Twitter API
     auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
     api = tweepy.API(auth)
-
-    # tweets = api.user_timeline(screen_name="nerdisland1728")
 
     tweeted = set()  # Create a set to store previously tweeted articles
 
